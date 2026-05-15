cc3d6d36c596f02473a0cd4db2791b9fd1017bc2
Zagreus-D-Git <hardbreakconque@gmail.com>
2026-05-14 21:46:44 -0600

    cuda binary cache update

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -19,8 +19,8 @@
         "https://cache.nixos-cuda.org/" # cuda compiled verificada
     ];
     trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkrJ16ZJo1..."
-      "nix-community.cachix.org-1:mB9F..."
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
     ];
 
